59e03d764a5dba715b6f157729dd7e280a3b0119
Your Name <you@example.com>
2024-12-13 22:58:39 -0300

    Creacion de contratos

---
+++ packages/hardhat/contracts/SimpleDEX.sol
@@ -0,0 +1,115 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+// ETH Kipu TP4
+// Autor L.N.R.
+
+/**
+    El contrato SimpleDEX debe contar obligatoriamente y sin modificación de la interface con las siguientes funciones:
+
+        - constructor(address _tokenA, address _tokenB)
+        - addLiquidity(uint256 amountA, uint256 amountB)
+        - swapAforB(uint256 amountAIn)
+        - swapBforA(uint256 amountBIn)
+        - removeLiquidity(uint256 amountA, uint256 amountB)
+        - getPrice(address _token)
+
+**/
+
+
+import "@openzeppelin/contracts@5.1.0/token/ERC20/IERC20.sol";
+
+contract SimpleDEX {
+
+    address owner;
+    IERC20 public tokenA;
+    IERC20 public tokenB;
+    uint256 public amountA;
+    uint256 public amountB;
+
+    uint256 public Ao;
+    uint256 public Bo;
+    uint256 public dA;
+    uint256 public dB;
+    uint256 public price;
+
+
+    constructor(address _tokenA, address _tokenB) { 
+        owner = msg.sender; // Seteo el owner como el que deploya el contrato
+
+        tokenA = IERC20 (_tokenA);
+        tokenB = IERC20 (_tokenB);
+    }
+
+    // MODIFICADORES
+    modifier onlyOwner {
+        require(msg.sender == owner, "Solo el owner puede ejecutar esta funcion");
+        _;
+    }
+
+    // FUNCIONES SOLICITADAS
+
+    function addLiquidity(uint256 _amountA, uint256 _amountB) public onlyOwner {
+        amountA = _amountA;
+        amountB = _amountB;
+    
+    // Cuando agrego liquidez tengo que hacerlo en pares.
+        tokenA.transferFrom(msg.sender, address(this), amountA);
+        tokenB.transferFrom(msg.sender, address(this), amountB);
+
+    }
+    function removeLiquidity(uint256 _amountA, uint256 _amountB) public onlyOwner {
+        amountA = _amountA;
+        amountB = _amountB;
+    // Saco del pool de liquidez y se lo devuelvo al owner
+        tokenA.transfer(msg.sender, amountA);
+        tokenB.transfer(msg.sender, amountB);
+    }
+
+    function swapAforB(uint256 amountAIn) public { 
+        // Utilizo la formula de liquidez continua 
+        // dY = Yo - (Xo * Yo) / (Xo + dX)
+        // Xo => Ao
+        // Yo => Bo
+        // dB = Bo - (Ao * Bo) / (Ao + dA)
+        dA = amountAIn;
+        Ao = tokenA.balanceOf(address(this));
+        Bo = tokenB.balanceOf(address(this));
+        dB = Bo - (Ao * Bo) / (Ao + dA);
+
+        tokenB.transferFrom(msg.sender, address(this), dB);
+        
+        
+    }
+    function swapBforA(uint256 amountBIn) public {
+        // Utilizo la formula de liquidez continua
+        // dX = (Xo * Yo) / (Yo - dY) - Xo
+        // Ao = Xo
+        // Bo = Yo
+        // dA = (Ao * Bo) / (Bo - dB) - Ao
+        dB = amountBIn;
+        Ao = tokenA.balanceOf(address(this));
+        Bo = tokenB.balanceOf(address(this));
+        dA = (Ao * Bo) / (Bo - dB) - Ao;
+
+        tokenA.transferFrom(msg.sender, address(this), amountA);    
+    
+    }
+    function getPrice(address _token) public { 
+        // Utilizo la formula de precio
+        // Precio A = Cantidad de B / Cantidad de A
+
+        Ao = tokenA.balanceOf(address(this)); // Cantidad de A
+        Bo = tokenB.balanceOf(address (this)); // Cantidad de B
+        // if (IERC20(_token) ==  tokenA)
+        if(_token == address(tokenA)){
+            price = Bo / Ao;
+        } else if (_token == address(tokenB)){
+            price = Ao / Bo;
+        } else revert("No se encuentra el token solicitado");
+        
+        price = price * 10**2; // Multiplico por la cantidad de decimales (dos en el caso de los Tokens)
+
+    }
+
+}
---
+++ packages/hardhat/contracts/TokenB.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.22;
+
+// ETH Kipu TP4
+// Autor L.N.R.
+
+import {ERC20} from "@openzeppelin/contracts@5.1.0/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts@5.1.0/access/Ownable.sol";
+
+contract TokenB is ERC20, Ownable {
+    constructor()
+        ERC20("TokenB", "TKB")
+        Ownable(msg.sender) // El owner va a ser el EOA que despliega el contrato
+    {
+        _mint(msg.sender, 10000 * 10 ** decimals()); // Minteo para el owner 10000 tokens
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function decimals() public pure override  returns (uint8) {
+        return 2; // Modifico la funcion para que tenga solo 2 decimales para conveniencia del TP
+    }
+
+
+}
